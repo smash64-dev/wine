6bc7f11f277959287092a15f1aa08a28f7d755af
CEnnis91 <cennis91@gmail.com>
2021-11-30 14:10:37 -0500

    libhidapi-0: add new library proxy stub (https://github.com/libusb/hidapi)

---
+++ dlls/libhidapi-0/unixlib.h
@@ -0,0 +1,29 @@
+/*
+ * Win32 based proxy to the hidapi library in Linux
+ *
+ * Copyright (c) 2021 Chris Ennis
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ *
+ * Original library from Alan Ott (Signal 11 Software)
+ * Maintained https://github.com/libusb/hidapi
+ */
+
+#include <stdarg.h>
+
+#include "windef.h"
+#include "winbase.h"
+
+#include "wine/unixlib.h"
